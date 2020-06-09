489b7b4ee04af1f243dc6c26b3e8a6a85b3b02d8
Idong Essien <idong.essien@comcast.net>
2020-06-09 19:20:22 -0400

    idongessien@idong-essien Sprint-Challenge--Intro-Python % python3 src/oop/test_oop2.py vroooom vroooom BRAAAP!! vroooom BRAAAP!! ..... ---------------------------------------------------------------------- Ran 5 tests in 0.000s
    OK

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -20,7 +20,12 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels = 2):
+        super().__init__(num_wheels)
 
+    def drive(self):
+        return "BRAAAP!!"
 
 vehicles = [
     GroundVehicle(),
@@ -33,3 +38,5 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+for motor in vehicles:
+    print(motor.drive())
